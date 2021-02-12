31835f3698a853cffd8cb219712ae996066c4ee2
Oscar Jiménez Aguilar <oscar.jimenez.ag@bykon.com.mx>
2021-02-12 14:56:36 -0600

    Update Main.java
    Se agrego una linea de codigo

--- src/com/jaos/firstexample/view/Main.java
+++ src/com/jaos/firstexample/view/Main.java
@@ -18,6 +18,7 @@ public class Main {
         
         System.out.println("Hola Mundo!!!");
         System.out.println("Que ta; Jovenes!!!");
+        System.out.println("Oscar Jimenez!!!");
         
         System.exit(0);
     }
